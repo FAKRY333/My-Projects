933edd284604de057f33489310b708eb3fe24499
Fakry <aaajjjdheid@gmail.com>
2025-10-02 01:47:33 +0300

    Delete projects/15- to do list directory

--- projects/15- to do list/list.js
+++
@@ -1,8 +0,0 @@
-opject = {
-    theName: 'fakry',
-    age: 16,
-    contry: 'egypt'
-}
- let {theName:n , age:a , contry:c} = opject ;
- console.log(n);
-window.localStorage.setItem('names', [ `the name is ${n}`, `the age is ` + a ,`the contry is `+ c , {sckills:['html', 'css']}]);
